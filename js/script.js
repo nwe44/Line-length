8d2d1b700dfaa9fe0fe351615a0905fc27ca999f
Nicholas Evans <nick@nick-evans.com>
2011-02-02 01:38:48 -0500

    Nightly commit

--- js/script.js
+++ js/script.js
@@ -1,40 +1,65 @@
-/* Author: 
+/* Author: Nick Evans
 
 */
 
 
 var ll = {}
 
-ll.countCharacters = function (){
+ll.countCharacters = function (newFontSize, myHeight){
+	myHeight |= $('#sample').height();
+	$('body').append('<div id="h-sample" style="font-size:' + newFontSize + '%;">ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwyxz</div>');
+	var lineHeight = $('#h-sample').height();
+ 	$('#h-sample').remove();
 
+	var noOfLines = parseInt(myHeight) / parseInt(lineHeight);
 	var mySample = document.getElementById('sample');
-	var allMyCharacters = mySample.getElementsByTagName('b');
-	var i = allMyCharacters.length;
-	var myLines = 0;
-	var myLineOffset = 0;
-	var currentOffset = {};
-	var myAverageLineLength = 0;
-	var currentCharacterWidth = 0;
-	while(i--) { 
-		currentOffset = $(allMyCharacters[i]).offset();
-		currentCharacterWidth =  $(allMyCharacters[i]).width();
-		if(currentOffset.top != myLineOffset && currentCharacterWidth > 0){
-			console.log("myLineOffset: " + myLineOffset + " vs " + "currentOffset.top: " +currentOffset.top );
-			myLineOffset = currentOffset.top;
-			myLines++;
-			$(allMyCharacters[i]).after("<span class='pipe'>|</span>");
-		}
-	}
-
-	
-	myAverageLineLength = parseInt(allMyCharacters.length / myLines);
-	$('#counter').html("Characters: " + allMyCharacters.length + "<br /> line(s):" + myLines + "<br />Average line length: " + myAverageLineLength);
+	var averageLineLength = parseInt(mySample.innerHTML.length / noOfLines);
+	var comment =	(averageLineLength < 10)  ? ", joker.":
+					(averageLineLength < 20)  ? ", sure you can't make it a bit longer?":
+					(averageLineLength < 40)  ? ", that's pretty snug.":
+					(averageLineLength < 90)  ? ".":
+					(averageLineLength < 100) ? ", careful now.":
+					(averageLineLength < 110) ? ", sure you know what you're doing?":
+					(averageLineLength < 120) ? ", really?":
+					(averageLineLength < 150) ? ", having fun?":
+												", ouch!";
+								
+	$('#counter').html("Your average line length is <strong>" + averageLineLength +"</strong>" +"<span class='comment'>" + comment+"</span>");
+	var backgroundLineHeight = parseInt(lineHeight) * 2;
+	$('#decoration').css("background-size", backgroundLineHeight  +'px ' + backgroundLineHeight  +'px');
+	$('#decoration').css("-moz-background-size", backgroundLineHeight  +'px ' + backgroundLineHeight  +'px');
+	$('#decoration').css("-webkit-background-size", backgroundLineHeight  +'px ' + backgroundLineHeight  +'px');
+	$('#decoration').animate({'height': $('#sample').css('height')}, 1000);
+	$('#length').html('<span>' + $('#sample').css('width') + '</span>').css('width', $('#sample').css('width'));
+	$('#sample-wrapper').css('width', $('#sample').css('width')).css('height', $('#sample').css('height'));
 }
+ll.behaviours = function(){
+	$('#sample').resizable({
+			grid: 10, 
+			handles: 'e',
+			containment: 'parent',
+			resize: function(event, ui) { 
+			$('#length').html('<span>' + $('#sample').css('width') + '</span>').css('width', $('#sample').css('width'));
+			$('#sample-wrapper').css('width', $('#sample').css('width')).css('height', $('#sample').css('height'));
+			},
+			stop: function(event, ui) { 
+				var myHeight = $('#sample').css("width", $(this).attr('value') + "px").height();
+				ll.countCharacters($('#type-size').attr('value'), myHeight);
+			} 
+	});
 
-
-
+	$('#type-size').change(function(){
+		var myHeight = $('#sample').css("font-size", $(this).attr('value') + "%").height();
+		ll.countCharacters($(this).attr('value'), myHeight);
+	});
+	$('#fonts').change(function(){
+		var myHeight = $('#sample').css("font-family","\"" + $(this).attr('value') + "\"").height();
+		ll.countCharacters($('#type-size').attr('value'), myHeight);
+	});
+}
 $(document).ready(function() { 
-	ll.countCharacters();
+	ll.countCharacters(100);
+	ll.behaviours();
 });
 
 
