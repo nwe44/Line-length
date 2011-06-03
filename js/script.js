cae4b1e2b7559c3b693ef20f7ea57f9f714ff1f1
Nicholas Evans <nick@nick-evans.com>
2011-06-03 17:43:32 -0400

    Tidy up for Public consumption.

--- js/script.js
+++ js/script.js
@@ -2,116 +2,189 @@
 
 ToDo
 Improve line length calculation
-design controls - maybe not as select boxes, but some sort of toggle button
-test resizable usability
-firefox not changing the background lines correctly
-why does this matter -- link to elements of typographic style applied to the web
-add typekit?
-add footer to cope #sample being shorter than the window
-add link to nick-evans.com
-branding?
 */
 
 
 var ll = {
-	getLineHeight : function(){
-		// remember, fonts are declared as percentages (we're using Yahoo css fonts)
-		// mozilla likes to do things like this, which makes sense to me.
-		var fontSize = parseInt($('#type-size').attr('value'));
-		
-		// I know browser sniffing is naughty, but this is a browser bug
-		if(WebKitDetect.isWebKit()){
-			$('body').append('<div id="h-sample" style="font-size:' + fontSize + '%;">ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwyxz</div>');
-			var lineHeight = $('#h-sample').height();
-		 	$('#h-sample').remove();
-		}else{
-			var lineHeight = parseInt($('#type-size').attr('value')) * (13/100) * 1.231;		
-		}
-		return lineHeight;
-	},
-	setRulerWidth : function(lineHeight){
-		var sampleWidth = $('#sample').css('width');
-		$('#length-span').html(sampleWidth).parent().css('width', sampleWidth);
-		$('#sample-wrapper').css('width', sampleWidth).css('height', (parseInt($('#sample').css('height')) + lineHeight) + "px");
-	},
-	countCharacters : function (newFontSize, myHeight){
-		myHeight |= $('#sample').height();
+	// First we store some variables
+	
+	averageLineMeasure : null,
 
-		lineHeight = ll.getLineHeight();	
-		var noOfLines = parseInt(myHeight) / parseInt(lineHeight);
-		var mySample = document.getElementById('sample');
-		var averageLineLength = parseInt(mySample.innerHTML.length / noOfLines);
+	lineHeight : null,
+
+	selector : null,
+	
+	noOfCharacters : null,
+
+	isWebKit : false,
+	
+	sampleHeight : null,
+	
+	measure : {
+
+		getLineHeight : function(){
+
+			// remember, fonts are declared as percentages (we're using Yahoo css fonts)
+			// mozilla likes to do things like this, which makes sense to me.
+			var fontSize = parseInt($('#type-size').attr('value'), 10);
+			
+			if(ll.isWebKit){ // ... but the above gives odd results in WebKit, as this isn't a feature to sniff, I'm happy doing a check for WebKit. 
+				$('body').append('<div id="h-sample" style="font-size:' + fontSize + '%;">ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwyxz</div>');
+				ll.lineHeight = $('#h-sample').height();
+				$('#h-sample').remove();
+			}else{
+				ll.lineHeight = parseInt($('#type-size').attr('value'), 10) * (13/100) * 1.231;		
+			}
+		}, // end getLineHeight
 		
-		var comment =	(averageLineLength < 10)  ? ", joker.":
-						(averageLineLength < 20)  ? ", watch out.":
-						(averageLineLength < 35)  ? ", that's pretty tight.":
-						(averageLineLength < 45)  ? ", getting snug.":
-						(averageLineLength < 75)  ? ". ":
-						(averageLineLength < 100) ? ", careful now.":
-						(averageLineLength < 110) ? ", know what you're doing?":
-						(averageLineLength < 120) ? ", seriously?":
-						(averageLineLength < 150) ? ", having fun?":
-													", ouch!";
-						
-		$('#characterCount').html(averageLineLength);
-		$('#comment').html(comment);
+		countCharacters : function(){
+			var mySample = document.getElementById(ll.selector.substr(1));
+			ll.noOfCharacters = mySample.innerHTML.length ;
+			
+		}, // end countCharacters
 		
-		// With the hard stuff done, let's do some decoration
+		lineMeasure : function (newFontSize, myHeight){
+
+			myHeight |= $(ll.selector).height();
+			
+			// To Do: strip out the trailing line, possibly by counting the position of injected spans.
+			var noOfLines = ll.sampleHeight / parseInt(ll.lineHeight, 10);
+
+			ll.averageLineMeasure = parseInt(ll.noOfCharacters / noOfLines, 10);
+
+		} // end lineMeasure		
+	
+	}, // end measure
+	
+	present : {
+	
+		comment : function () {
+
+			var myComment = (ll.averageLineMeasure < 10)  ? ", joker.":
+							(ll.averageLineMeasure < 20)  ? ", watch out.":
+							(ll.averageLineMeasure < 35)  ? ", that's pretty tight.":
+							(ll.averageLineMeasure < 45)  ? ", getting snug.":
+							(ll.averageLineMeasure < 75)  ? ". ":
+							(ll.averageLineMeasure < 100) ? ", careful now.":
+							(ll.averageLineMeasure < 110) ? ", know what you're doing?":
+							(ll.averageLineMeasure < 120) ? ", seriously?":
+							(ll.averageLineMeasure < 150) ? ", having fun?":
+														", ouch!";
+														
+			$('#characterCount').html(ll.averageLineMeasure + " ");
+
+			$('#comment').html(myComment);
+
+		}, // end comment
 		
-		// Grab the line heights and adjust them to look visually correct
-		var backgroundLineHeight = lineHeight * 2;
-		
-		// I'd like to declare background size in ems in css, and just change typesize here, but webkit makes this impossible
-		if(WebKitDetect.isWebKit()){
+		decorate : function (lineHeight) {
+				
+			// Grab the line heights and adjust them to look visually correct
+			var backgroundLineHeight = lineHeight * 2;
+			
+			// I'd like to declare background size in ems in css and just change typesize here, but webkit makes this impossible
+			if(ll.isWebKit){
+					$('#decoration').css("background-size", backgroundLineHeight  +'px ' + backgroundLineHeight  +'px')
+							.css("-webkit-background-size", backgroundLineHeight  +'px ' + backgroundLineHeight  +'px');		
+			}else{
 				$('#decoration').css("background-size", backgroundLineHeight  +'px ' + backgroundLineHeight  +'px')
-						.css("-webkit-background-size", backgroundLineHeight  +'px ' + backgroundLineHeight  +'px');		
-		}else{
-			$('#decoration').css("background-size", backgroundLineHeight  +'px ' + backgroundLineHeight  +'px')
-						.css("-moz-background-size", backgroundLineHeight  +'px ' + backgroundLineHeight  +'px');
-		}
+							.css("-moz-background-size", backgroundLineHeight  +'px ' + backgroundLineHeight  +'px');
+			}
+	
+			// Change the body height to accomodate the text
+			if(parseInt($(ll.selector).height(), 10) + 130 < parseInt($(window).height(), 10)){
 
-		// Change the body height to accomodate the text
-		if(parseInt($('#sample').height()) + 130 < parseInt($(window).height())){
-			$('#decoration').animate({'height': ($(window).height()-150) + "px"}, 1000);
-		}else{
-			$('#decoration').animate({'height': $('#sample').css('height')}, 1000);
-		}
-		ll.setRulerWidth(lineHeight);
-	},
+				$('#decoration').animate({'height': ($(window).height()-150) + "px"}, 1000);
 
-	behaviours : function(){
-		$('#sample').resizable({
+			}else{
+
+				$('#decoration').animate({'height': $(ll.selector).css('height')}, 1000);
+
+			}
+
+		}, // end decorate
+		
+		
+		// changes the shape of the grabber
+		setRulerDimensions : function(){
+			var sampleWidth = $(ll.selector).css('width');
+			$('#length-span').html(sampleWidth).parent().css('width', sampleWidth);
+			$('#sample-wrapper').css('width', sampleWidth).css('height', (parseInt($(ll.selector).css('height'), 10) + ll.lineHeight) + "px");
+		} // end setRulerDimensions
+	
+	}, // end present
+
+
+	logic : {
+		assess : function () {
+			
+			// update the lineHeight
+			ll.measure.getLineHeight();
+		
+			// Find out what we're working with
+			ll.measure.lineMeasure();
+			
+			// update the comment
+			ll.present.comment();
+			
+			// decorate the background
+			ll.present.decorate(ll.lineHeight);
+			
+			// change the number on the slider, and the grabber shape
+			ll.present.setRulerDimensions(ll.lineHeight);				
+			
+		} // end assess
+	}, // end logic
+
+	init : function(selector){
+		// begin by defining our environment
+		
+		ll.selector = selector || '#sample';
+		
+		ll.sampleHeight = parseInt($(ll.selector).height(), 10);
+		
+		// find out the size of our sample
+		ll.measure.countCharacters();
+		
+		// are we using webKit
+		// I know browser sniffing is naughty, but this is a browser bug, so I reason this is acceptible
+		ll.isWebkit = RegExp(" AppleWebKit/").test(navigator.userAgent);
+		
+		
+		// setup behaviours
+		$(ll.selector).resizable({
 				grid: 10, 
 				handles: 'e',
 				containment: 'parent',
 				resize: function(event, ui) { 
-					ll.setRulerWidth(ll.getLineHeight());
-					$('.ui-resizable-e').css('width', ($('#sample').width() + 30)+"px");
-					if($('#decoration').height() < $('#sample').height()){
-						$('#decoration').css('height', $('#sample').css('height'));
+					ll.present.setRulerDimensions();
+					$('.ui-resizable-e').css('width', ($(ll.selector).width() + 30)+"px");
+					if($('#decoration').height() < $(ll.selector).height()){
+						$('#decoration').css('height', $(ll.selector).css('height'));
 					}
 				},
 				stop: function(event, ui) { 
-					var myHeight = $('#sample').css("width", $(this).attr('value') + "px").height();
-					ll.countCharacters($('#type-size').attr('value'), myHeight);
+					ll.sampleHeight = $(ll.selector).css("width", $(this).attr('value') + "px").height();
+					ll.logic.assess();
 				} 
 		});
-		$('.disqus-toggle').click(function(){
-			$('#disqus-wrapper').slideToggle();
-		});
 		$('#type-size').change(function(){
-			var myHeight = $('#sample').css("font-size", $(this).attr('value') + "%").height();
-			ll.countCharacters($(this).attr('value'), myHeight);
+			ll.sampleHeight = $(ll.selector).css("font-size", $(this).attr('value') + "%").height();
+			ll.logic.assess();
 		});
 		$('#fonts').change(function(){
-			var myHeight = $('#sample').css("font-family","\"" + $(this).attr('value') + "\"").height();
-			ll.countCharacters($('#type-size').attr('value'), myHeight);
+			ll.sampleHeight = $(ll.selector).css("font-family","\"" + $(this).attr('value') + "\"").height();
+			ll.logic.assess();
 		});
-	}
-}
+		
+		
+		// check the initial setup
+		ll.logic.assess();
+	}// end init
+};
+
 $(document).ready(function() { 
-	ll.countCharacters(100);
-	ll.behaviours();
+	ll.init();
 });
 
 
